d4041d6434ba16e1fff0f405e06ce69e771dc965
Kristijan <kkelic7@gmail.com>
2020-01-23 00:23:29 +0100

    creating logo, makin header component

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import './App.scss';
+
 function App() {
   return (
-    <div>
+    <div className="App">
     </div>
   );
 }
---
+++ to-do/src/components/header/header.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
+import './header.component.scss';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <Logo className="logo" />
+      <div className="title">
+        <h4>TO DO</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
