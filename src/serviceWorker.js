b38cef411b3ec85191b1a135117c68bea9ecae32
Kristijan <kkelic7@gmail.com>
2020-01-29 00:41:44 +0100

    changing manifest background color

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -22,6 +22,19 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    //MOJE
+    window.addEventListener('activate', function(event) {
+      event.waitUntil(
+        caches.keys().then(function(caches) {
+          return Promise.all(
+            caches.keys().then(function(names) {
+              for (let name of names) caches.delete(name);
+            })
+          );
+        })
+      );
+    });
+
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
