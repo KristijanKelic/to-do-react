3a89d7a06edbb4aac2b6799d36c36f11a4f71f4c
Kristijan <kkelic7@gmail.com>
2020-01-28 17:49:09 +0100

    moving around

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import * as serviceWorker from './serviceWorker';
@@ -12,11 +12,11 @@ import { store, persistor } from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
