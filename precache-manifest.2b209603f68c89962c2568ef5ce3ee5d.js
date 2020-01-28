20d5cd940936347649cd216a00c9d0de29d6af20
Kristijan <kkelic7@gmail.com>
2020-01-28 17:30:53 +0100

    Updates

---
+++ precache-manifest.2b209603f68c89962c2568ef5ce3ee5d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a0f62a174d4e9a0793e97eb40ff4715",
+    "url": "/KristijanKelic/to-do-react/to-do/index.html"
+  },
+  {
+    "revision": "d9b43b894600427d2a5b",
+    "url": "/KristijanKelic/to-do-react/to-do/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "0ceb08c0440e9de306d3",
+    "url": "/KristijanKelic/to-do-react/to-do/static/css/main.03cc68ef.chunk.css"
+  },
+  {
+    "revision": "d9b43b894600427d2a5b",
+    "url": "/KristijanKelic/to-do-react/to-do/static/js/2.c61c0e9e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/KristijanKelic/to-do-react/to-do/static/js/2.c61c0e9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ceb08c0440e9de306d3",
+    "url": "/KristijanKelic/to-do-react/to-do/static/js/main.5d716318.chunk.js"
+  },
+  {
+    "revision": "60b409093cfb97bfacb0",
+    "url": "/KristijanKelic/to-do-react/to-do/static/js/runtime-main.29bc1eb8.js"
+  },
+  {
+    "revision": "79404ac47b8c0e8ca6bc3d34554bb6cb",
+    "url": "/KristijanKelic/to-do-react/to-do/static/media/add.79404ac4.svg"
+  },
+  {
+    "revision": "4b8e86909a3f40c47f3703b766b8714d",
+    "url": "/KristijanKelic/to-do-react/to-do/static/media/delete.4b8e8690.svg"
+  },
+  {
+    "revision": "327958fd2ef3c233783c12740c6ce372",
+    "url": "/KristijanKelic/to-do-react/to-do/static/media/information.327958fd.svg"
+  },
+  {
+    "revision": "65521e7a1bcf495869280e58b8a7ab82",
+    "url": "/KristijanKelic/to-do-react/to-do/static/media/list.65521e7a.svg"
+  },
+  {
+    "revision": "c0cbd0847616c5b88e858a5e2abacdbe",
+    "url": "/KristijanKelic/to-do-react/to-do/static/media/tick.c0cbd084.svg"
+  },
+  {
+    "revision": "b5fa2cbc5fa4fe6b23dcaf7efa8a1b15",
+    "url": "/KristijanKelic/to-do-react/to-do/static/media/to-do-list.b5fa2cbc.svg"
+  },
+  {
+    "revision": "870a5694b10e508084d7ae1a39c1f665",
+    "url": "/KristijanKelic/to-do-react/to-do/static/media/undo.870a5694.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/KristijanKelic/to-do-react/precache-manifest.f4053c656968003f7d2c3fa55cc19fb6.js"
+  "/KristijanKelic/to-do-react/to-do/precache-manifest.2b209603f68c89962c2568ef5ce3ee5d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KristijanKelic/to-do-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/KristijanKelic/to-do-react/to-do/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
