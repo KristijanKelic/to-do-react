a12e26b94208358cc0808b64f85f47ffbc1c4910
Kristijan <kkelic7@gmail.com>
2020-01-28 17:27:46 +0100

    test build

---
+++ to-do/build/precache-manifest.f4053c656968003f7d2c3fa55cc19fb6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ac55ecc491592b5f63e98c331b9bbd",
+    "url": "/KristijanKelic/to-do-react/index.html"
+  },
+  {
+    "revision": "d9b43b894600427d2a5b",
+    "url": "/KristijanKelic/to-do-react/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "e56c3ee6ca3e634ed144",
+    "url": "/KristijanKelic/to-do-react/static/css/main.03cc68ef.chunk.css"
+  },
+  {
+    "revision": "d9b43b894600427d2a5b",
+    "url": "/KristijanKelic/to-do-react/static/js/2.c61c0e9e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/KristijanKelic/to-do-react/static/js/2.c61c0e9e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e56c3ee6ca3e634ed144",
+    "url": "/KristijanKelic/to-do-react/static/js/main.502bdc0d.chunk.js"
+  },
+  {
+    "revision": "216389c2c4ee6c5c25e9",
+    "url": "/KristijanKelic/to-do-react/static/js/runtime-main.e704c9f0.js"
+  },
+  {
+    "revision": "79404ac47b8c0e8ca6bc3d34554bb6cb",
+    "url": "/KristijanKelic/to-do-react/static/media/add.79404ac4.svg"
+  },
+  {
+    "revision": "4b8e86909a3f40c47f3703b766b8714d",
+    "url": "/KristijanKelic/to-do-react/static/media/delete.4b8e8690.svg"
+  },
+  {
+    "revision": "327958fd2ef3c233783c12740c6ce372",
+    "url": "/KristijanKelic/to-do-react/static/media/information.327958fd.svg"
+  },
+  {
+    "revision": "65521e7a1bcf495869280e58b8a7ab82",
+    "url": "/KristijanKelic/to-do-react/static/media/list.65521e7a.svg"
+  },
+  {
+    "revision": "c0cbd0847616c5b88e858a5e2abacdbe",
+    "url": "/KristijanKelic/to-do-react/static/media/tick.c0cbd084.svg"
+  },
+  {
+    "revision": "b5fa2cbc5fa4fe6b23dcaf7efa8a1b15",
+    "url": "/KristijanKelic/to-do-react/static/media/to-do-list.b5fa2cbc.svg"
+  },
+  {
+    "revision": "870a5694b10e508084d7ae1a39c1f665",
+    "url": "/KristijanKelic/to-do-react/static/media/undo.870a5694.svg"
+  }
+]);
